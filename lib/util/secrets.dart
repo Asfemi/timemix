21dba852491569d1eea1ab02f614f1139c874063
asfemi <pearlcstall@gmail.com>
2023-01-09 13:38:33 +0100

    heyy

--- lib/util/secrets.dart
+++ lib/util/secrets.dart
@@ -1,7 +1,7 @@
 import 'dart:io' show Platform;
 
 class Secret {
-  static const ANDROID_CLIENT_ID = "565278034841-96emjjgi2tq9su8mkcm14deguhludcjd.apps.googleusercontent.com";
+  static const ANDROID_CLIENT_ID = "<565278034841-96emjjgi2tq9su8mkcm14deguhludcjd.apps.googleusercontent.com>";
   //todo: setup ios path and add the ios client secret
   static const IOS_CLIENT_ID = "<enter your iOS client secret>";
   static String getId() => Platform.isAndroid ? Secret.ANDROID_CLIENT_ID : Secret.IOS_CLIENT_ID;
